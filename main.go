1bade92356e7f9d130ef40434b26b0deef18cd8d
achilles_xushy <yuqingxushiyin@gmail.com>
2019-03-14 10:01:22 +0800

    functions

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"sort"
@@ -98,6 +99,36 @@ func parseHtml() {
 	for _, link := range visit(nil, doc) {
 		fmt.Println(link)
 	}
+}
+
+func findLinksPrint() {
+	for _, url := range os.Args[:1] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s:%s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML :%v", url, err)
+	}
+	return visit(nil, doc), nil
 }
 
 func visit(links []string, n *html.Node) []string {
@@ -292,4 +323,3 @@ func addNode(r *tree, v int) *tree {
 	}
 	return r
 }
-
